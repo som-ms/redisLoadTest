bb661ce8dbbc31c4f66031610a9823d5fd7d17d6
Som Dutt <somdutt@microsoft.com>
2021-06-21 12:07:42 +0530

    commit triggers

--- server/trigger.js
+++ server/trigger.js
@@ -3,14 +3,14 @@ const { spawn } = require('child_process');
 const channelName = Math.random().toString(36).substring(7);
 console.log("channel Name: " + channelName)
 for (let i = 0; i < 10; i++) {
-    spawn('node', ['./server/subscriber.js', channelName, i]);
-    console.log("subscriber started" + i)
+    spawn('node', ['./subscriber.js', channelName, i]);
+    //console.log("subscriber started" + i)
 }
 
 
 setTimeout(startPublisher, 20000);
 
 function startPublisher(){
-    spawn('node', ['./server/publisher.js', channelName, 30]);  // time to publish
-    console.log("publisher started")
+    spawn('node', ['./publisher.js', channelName, 35]);  // time to publish
+    //console.log("publisher started")
 }
